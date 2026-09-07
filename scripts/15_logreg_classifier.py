89fa3635543ab9b149d2f421d0434d05daa9bfbb
Gulbanu Mukhanbetkali <gulbanum1@gmail.com>
2026-09-07 09:40:38 -0400

    Update 15_logreg_classifier.py
    path changed

--- scripts/15_logreg_classifier.py
+++ scripts/15_logreg_classifier.py
@@ -22,11 +22,11 @@ from sklearn.metrics import (
 )
 import optuna
 
-X_train_lin = pd.read_csv("/Workspace/Capstone_Group1/processed/X_train_linear.csv")
-X_test_lin  = pd.read_csv("/Workspace/Capstone_Group1/processed/X_test_linear.csv")
-y_train_clf = pd.read_csv("/Workspace/Capstone_Group1/processed/y_train_clf.csv").iloc[:, 0]
-y_test_clf  = pd.read_csv("/Workspace/Capstone_Group1/processed/y_test_clf.csv").iloc[:, 0]
-train_raw   = pd.read_csv("/Workspace/Capstone_Group1/processed/train_temporal.csv")
+X_train_lin = pd.read_csv("processed/X_train_linear.csv")
+X_test_lin  = pd.read_csv("processed/X_test_linear.csv")
+y_train_clf = pd.read_csv("processed/y_train_clf.csv").iloc[:, 0]
+y_test_clf  = pd.read_csv("processed/y_test_clf.csv").iloc[:, 0]
+train_raw   = pd.read_csv("processed/train_temporal.csv")
 
 fire_ids = train_raw["ID"]
 print(f"Unique fires in train: {fire_ids.nunique()}")
